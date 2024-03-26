b8670403e28386eb5f5222927e4ff4b80b2754ac
Edward Dudas <edwarddudas@gmail.com>
2024-03-26 19:43:57 +0200

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+// // HW 1
+let arr = [1,2,3,4,5];
+for(let i = 1; i<=arr.length; i++){
+
+    console.log(i);
+}
+
+// // HW 2
+let array = [1,2,3,4,5];
+let sum = 0;
+for(let a = 0; a<array.length; a++){
+    console.log(sum += array[a]);
+}
+
+// HW 3
+let num = [1, 2, 3, 4, 5];
+console.log(Math.max(...num))
